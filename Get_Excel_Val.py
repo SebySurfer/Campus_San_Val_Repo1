956fc03924faf2e177822f79c361f994c5676ef0
SebySurfer <sebysurfer@gmail.com>
2024-02-14 15:41:42 -0600

    Main Branch

---
+++ Get_Excel_Val.py
@@ -0,0 +1,19 @@
+import openpyxl
+
+# Load the Excel workbook
+workbook = openpyxl.load_workbook('Book1.xlsx')
+# Select the worksheet you want to work with
+worksheet = workbook['Sheet1']
+
+# Specify the coordinates of the cell you want to retrieve
+cell_coordinate = 'B2'
+
+# Get the value of the specified cell
+cell_value = worksheet[cell_coordinate].value
+
+# Print the value
+print("Value of cell", cell_coordinate, ":", cell_value)
+
+
+x = worksheet['D7'].value
+print("Random ass value", x)
