64b8eedecb06892fc97e3d6f340a68c543564b25
SebySurfer <sebysurfer@gmail.com>
2024-02-15 13:31:14 -0600

    Commit before solution to ordering matches

--- Main.py
+++ Main.py
@@ -38,7 +38,13 @@ def print_Res(client):
     print("Client ", client[0], ", ", client[3],"4",client[4], ". WhatsApp: ", client[2])
     print("Results: ")
 
+def print_Partner(partner):
+    print("Partner: ", partner[0], " Social: ", partner[2], " Match Rate: ", partner[41], "% ")
 
+
+# I need to find a way to identify the index of every girl when rearranging the dam All_scores
+        # Solution: Every client will have the match rate at the end of their array, that way they are
+        # arranged based on the value of their index.
 
 def alg_Straight(list_m, list_f):
 
@@ -52,16 +58,11 @@ def alg_Straight(list_m, list_f):
     value_m = 0
     value_f = 0
 
-    top_5 = [0] * 5
-    top_2 = [0] * 2
-
-
-
     #First iterate every client of every list
     for male in list_m:
         for female in list_f:
-            for index in range(10, 40):
-
+            #index from 10 to 40
+            for index in range(10, 41):
                 #Valores Fundamentales
                 if(index < 34):
                     value_m = male[index] * male[index+1]
@@ -75,21 +76,56 @@ def alg_Straight(list_m, list_f):
                         match_score += PF_pts
                     index+= 1 #To skip the follow-up question
 
+                    #Reset values to use them again
+                    value_m = 0
+                    value_f = 0
+
                 #Valores Especificos
                 elif(index >= 34):
                     if(male[index] == female[index]):
-                        match_score += PF_pts
+                        match_score += PE_pts
 
-        All_Scores.append(match_score)
+            # Index 41 = added space for match rate
+            female[41] = match_score
 
-    #Using method from library to sort All_Scores from highest to lowest
-    All_Scores.sort(reverse=True)
+            #Adding for each female their match score towards the list.
+            #RESOLVE ISSUE: how is it possible to organize an array of lists, from highest to lowest, based on a specific index that is the same for every other list ?
+            All_Scores.append(female)
 
-    #I need to find a way to identify the index of every girl when rearranging the dam All_scores
-    #Solution: Every client will have the match rate at the end of their array, that way they are
-    # arranged based on the value of their index.
 
-    print_Res(male)
+        #Print results for each male, by using method from library to sort All_Scores from highest to lowest
+        All_Scores.sort(reverse=True)
+
+
+        print_Res(male)
+        #First printing the top 3 scores, considering their careers
+        # index 7 is your career
+        # index 8 and 9 are the eliminated careers
+
+        count3 = 0
+        count2 = 0
+
+        print("Best matches:")
+        for c in All_Scores:
+            for index in range(len(All_Scores)):
+                if(count3 < 4) and (index < len(All_Scores)):
+                    if(male[7] != c[8]) or (male[7] != c[9]):
+                        print_Partner(c)
+                        count3+= 1
+
+        print("Matches that you made, but you excluded them for career type")
+        for c in All_Scores:
+            for index in range(len(All_Scores)):
+                if (count2 < 2) and (index < len(All_Scores)):
+                    if (male[7] == c[8]) or (male[7] == c[9]):
+                        count2 += 1
+
+        #Resets match for new partners
+        female[41] = 0
+
+
+
+
 
 
 
