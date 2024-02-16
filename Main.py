02e2ed5391822972c3087b307abfaeaf37b75be1
SebySurfer <sebysurfer@gmail.com>
2024-02-15 21:28:35 -0600

    Finished changes before adding excel data & sorting

--- Main.py
+++ Main.py
@@ -16,23 +16,21 @@ main_List = []
 
 #All Lists
 
-gay_cas_list = [] #Add bisexual males
-gay_ser_list = [] #Add bisexual males
+gay_list = [] #Add bisexual males
 
-lesb_cas_list = [] #Add bisexual females
-lesb_ser_list = [] #Add bisexual females
-
+lesb_list = [] #Add bisexual females
 
 straight_cas_list_m = []
 straight_cas_list_f = [] #Adding bisexual females
-
-straight_ser_list_m = []
-straight_ser_list_f = []
 
 #Theory: casual relationships dont care about political standpoint
 #For this, i'm not going to include it in the algorithm and see how it goes
 # (its a trial test run)
 
+#Side-Note: there's an abundance of straigh_cas_males, so no need to add bi males in the list
+
+straight_ser_list_m = []
+straight_ser_list_f = []
 
 
 #For-Loop to add all the objects to every list
@@ -47,8 +45,10 @@ def print_Res(client):
     print("Client ", client[0], ", ", client[3],"4",client[4], ". WhatsApp: ", client[2])
     print("Results: ")
 
-def print_Partner(partner):
-    print("Partner: ", partner[0], " Social: ", partner[2], " Match Rate: ", partner[41], "% ")
+def print_Partner(client, partner):
+    #This solves for the same-sex lists
+    if(client != partner):
+        print("Partner: ", partner[0], " Social: ", partner[2], " Match Rate: ", partner[41], "% ")
 
 
 # I need to find a way to identify the index of every girl when rearranging the dam All_scores
@@ -83,7 +83,7 @@ def alg_Straight(list_m, list_f):
                     #Reset values to use them again
                     value_m = 0
                     value_f = 0
-            #index from 34 to 39 - Preguntas Especificas - Gatos/Perros
+            #index from 34 to 39 - Preguntas Especificas (-Gatos/Perros)
             for index in range(34, 40):
                 if (male[index] < 0) and (female[index] < 0):
                     match_score += PE_pts
@@ -104,34 +104,35 @@ def alg_Straight(list_m, list_f):
             # Index 41 = added space for match rate
             female[41] = match_score
 
-            #Adding for each female their match score towards the list.
+        #Organizing an array of lists, from highest to lowest,
+        #based on a specific index that is the same for every other list
 
         index_to_sort = 41
-        # RESOLVE ISSUE: how is it possible to organize an array of lists, from highest to lowest, based on a specific index that is the same for every other list ?
+
         All_Scores = sorted(list_f, key=lambda x: x[index_to_sort], reverse=True)
 
         print_Res(male)
-        #First printing the top 3 scores, considering their careers
         # index 7 is your career
         # index 8 and 9 are the eliminated careers
 
         count3 = 0
         count2 = 0
 
+        #First printing the top 3 scores, considering their careers to eliminate
         print("Best matches:")
         for w in All_Scores:
             for index in range(len(All_Scores)):
                 if(male[8] != w[7]) or (male[9] != w[7]):
-                    print_Partner(w)
+                    print_Partner(male, w)
                     count3 += 1
                 if(count3 == 3):
                     break
-
+        #Second, printing the top 2 scores, getting the excluded careers
         print("Matches that you made, but you excluded them for career type")
         for w in All_Scores:
             for index in range(len(All_Scores)):
                 if (male[7] == w[8]) or (male[7] == w[9]):
-                    print_Partner(w)
+                    print_Partner(male, w)
                     count2 += 1
                 if(count2 == 2):
                     break
@@ -140,19 +141,33 @@ def alg_Straight(list_m, list_f):
         female[41] = 0
         All_Scores = []
 
+
+print("List of serious-straight couples")
+print("---------------------------------")
 #Using Algorithm for serious-straight couples
-#
 print("List for men: ")
 alg_Straight(straight_ser_list_m, straight_ser_list_f)
 print("List for women: ")
 alg_Straight(straight_ser_list_f, straight_ser_list_m)
 
+print("List of casual-straight couples")
+print("---------------------------------")
 #Algorithm for casual-straight couples
 print("List for men: ")
 alg_Straight(straight_cas_list_m, straight_cas_list_f)
 print("List for women: ")
 alg_Straight(straight_cas_list_f, straight_cas_list_m)
 
+print("List for gays")
+print("---------------------------------")
+#Algorithm for gays
+#From lack of this number, i have decided to have both casual and serious together for the LG community
+alg_Straight(gay_list, gay_list)
+
+print("List for lesbians")
+print("---------------------------------")
+#Algorithm for lesb
+alg_Straight(lesb_list, lesb_list)
 
 
 
